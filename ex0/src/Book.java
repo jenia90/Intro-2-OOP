bce6c57ea70a483961405b29098b99a8d8fba3e7
Jenia <genia90@gmail.com>
2016-02-25 00:14:59 +0200

    initial ex0 module commit

--- ex0/src/Book.java
+++ ex0/src/Book.java
@@ -1,5 +1,39 @@
-/**
- * Created by jenia on 25/02/2016.
- */
 public class Book {
+    String _bookTitle;
+    String _bookAuthor;
+    int _bookYearOfPublication;
+    int _bookComicValue;
+    int _bookDramaticValue;
+    int _bookEducationalValue;
+
+    Book(String bookTitle, String bookAuthor, int bookYearOfPublication,
+         int bookComicValue, int bookDramaticValue, int bookEducationalValue){
+
+        _bookTitle = bookTitle;
+        _bookAuthor = bookAuthor;
+        _bookYearOfPublication = bookYearOfPublication;
+        _bookComicValue = bookComicValue;
+        _bookDramaticValue = bookDramaticValue;
+        _bookEducationalValue = bookEducationalValue;
+    }
+
+    private int getCurrentBorrowerId(){
+        return 0;
+    }
+
+    private int getLiteraryValue(){
+        return 0;
+    }
+
+    private void returnBook(){
+
+    }
+
+    private void setBorrowerId(int borrowerId){
+
+    }
+
+    private String stringRepresentation(){
+        return String.format("[%s,%s,%d]", _bookTitle, _bookAuthor, getLiteraryValue());
+    }
 }
