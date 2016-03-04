30bb0b3019671cbf045aafa9d3a792a98937199b
Jenia <genia90@gmail.com>
2016-03-04 18:44:39 +0200

    Initial ex1 submit

---
+++ ex1/src/Competition.java
@@ -0,0 +1,80 @@
+/**
+ * The Competition class represents a Nim competition between two players, consisting of a given number of rounds. 
+ * It also keeps track of the number of victories of each player.
+ */
+public class Competition {
+	private final Player mPlayer1;
+    private final Player mPlayer2;
+    private final boolean mDisplayMessage;
+
+	/* You need to implement this class */
+	public Competition(Player player1, Player player2, boolean displayMessage){
+        mPlayer1 = player1;
+        mPlayer2 = player2;
+        mDisplayMessage = displayMessage;
+	}
+
+    public int getPlayerScore(int playerPosition){
+
+    }
+
+    public void playMultiple(int numRounds){
+
+    }
+	
+	/*
+	 * Returns the integer representing the type of player 1; returns -1 on bad
+	 * input.
+	 */
+	private static int parsePlayer1Type(String[] args){
+		try{
+			return Integer.parseInt(args[0]);
+		} catch (Exception E){
+			return -1;
+		}
+	}
+	
+	/*
+	 * Returns the integer representing the type of player 2; returns -1 on bad
+	 * input.
+	 */
+	private static int parsePlayer2Type(String[] args){
+		try{
+			return Integer.parseInt(args[1]);
+		} catch (Exception E){
+			return -1;
+		}
+	}
+	
+	/*
+	 * Returns the integer representing the type of player 2; returns -1 on bad
+	 * input.
+	 */
+	private static int parseNumberOfGames(String[] args){
+		try{
+			return Integer.parseInt(args[2]);
+		} catch (Exception E){
+			return -1;
+		}
+	}
+
+	/**
+	 * The method runs a Nim competition between two players according to the three user-specified arguments. 
+	 * (1) The type of the first player, which is a positive integer between 1 and 4: 1 for a Random computer
+	 *     player, 2 for a Heuristic computer player, 3 for a Smart computer player and 4 for a human player.
+	 * (2) The type of the second player, which is a positive integer between 1 and 4.
+	 * (3) The number of rounds to be played in the competition.
+	 * @param args an array of string representations of the three input arguments, as detailed above.
+	 */
+	public static void main(String[] args) {
+		
+		int p1Type = parsePlayer1Type(args);
+		int p2Type = parsePlayer2Type(args);
+		int numGames = parseNumberOfGames(args);
+		
+        Board board = new Board();
+
+		
+	}	
+	
+}
---
+++ ex1/src/Move.java
@@ -0,0 +1,13 @@
+/**
+ * Created by jenia on 02/03/2016.
+ */
+public class Move {
+    public Move(){
+
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
