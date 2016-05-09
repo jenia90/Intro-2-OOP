036465a8f161eb7cae86f37d8dbd7c098bd68dfa
Jenia <genia90@gmail.com>
2016-05-09 22:23:38 +0300

    submitted!

--- ex4/src/data_structures/oop/ex4/data_structures/AvlTree.java
+++ ex4/src/data_structures/oop/ex4/data_structures/AvlTree.java
@@ -1,11 +1,10 @@
 package oop.ex4.data_structures;
 
-import java.awt.*;
-import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 /**
- * Created by jenia on 08/04/2016.
+ * An implementation of the AVL tree data structure.
  */
 public class AvlTree implements Iterable<Integer> {
 
@@ -14,31 +13,56 @@ public class AvlTree implements Iterable<Integer> {
     private int size = 0;
     private AvlTreeNode rootNode;
 
+    /**
+     * Default constructor
+     */
     public AvlTree(){
         rootNode = null;
     }
 
-    public AvlTree(AvlTree avlTree){
-        this();
-
-        rootNode = new AvlTreeNode(avlTree.getRoot().getValue(), avlTree, null);
-        size = avlTree.size();
+    /**
+     * A copy constructor that creates a deep copy of the given AvlTree.
+     * @param tree The AVL tree to be copied.
+     * @throws NullPointerException
+     */
+    public AvlTree(AvlTree tree) throws NullPointerException{
+        this(tree.inOrderTraversal(tree.rootNode, new int[tree.size()], 0));
     }
 
-    public AvlTree(int[] data){
+    /**
+     * A constructor that builds a new AVL tree containing all unique values in the input array.
+     * @param data the values to add to tree.
+     * @throws NullPointerException
+     */
+    public AvlTree(int[] data) throws NullPointerException{
         this();
 
+        if (data == null) throw new NullPointerException();
+
         try {
-            for (int val :
-                    data) {
+            for (int val : data) {
                 add(val);
             }
-        } catch (NullPointerException ex){
-            System.out.println(ex.getMessage());
+        } catch (NullPointerException e){
+            e.printStackTrace();
+            System.err.println(e.getMessage());
         }
 
     }
 
+    /**
+     * Sets the rootNode of the tree
+     * @param newRoot the new rootNode.
+     */
+    protected void setRoot(AvlTreeNode newRoot){
+        rootNode = newRoot;
+    }
+
+    /**
+     * Add a new node with the given value to the tree.
+     * @param newValue the value of the new node to add.
+     * @return true if the value to add is not already in the tree and it was successfully added, false otherwise.
+     */
     public boolean add(int newValue){
         if (findNode(newValue) != null)
             return false;
@@ -53,6 +77,11 @@ public class AvlTree implements Iterable<Integer> {
         return true;
     }
 
+    /**
+     * Recursive helper method for the add method.
+     * @param node the node that the new value node should be added to.
+     * @param value the value to be added
+     */
     private void addTo(AvlTreeNode node, int value){
         if(value < node.getValue()){
             AvlTreeNode left = node.getLeftChildNode();
@@ -71,26 +100,26 @@ public class AvlTree implements Iterable<Integer> {
                 addTo(right, value);
             }
         }
+
         node.balanceNode();
     }
 
-    protected AvlTreeNode getRoot(){
-        return rootNode;
-    }
-
-    protected void setRoot(AvlTreeNode newRoot){
-        rootNode = newRoot;
-    }
-
+    /**
+     * Check whether the tree contains the given input value.
+     * @param searchVal value to search for
+     * @return if val is found in the tree, return the depth of the node (0 for the root) with the given value
+     * if it was found in the tree, -1 otherwise.
+     */
     public int contains(int searchVal){
+        // first we find the node in question.
         AvlTreeNode node = findNode(searchVal);
         int height = 0;
 
+
         if(node != null) {
-            FromFilesTester.printTree();
             AvlTreeNode currentNode = rootNode;
 
-
+            // if it exists we take the path to it and increment the height variable with each step
             while(currentNode != null){
                 if(searchVal < currentNode.getValue()){
                     currentNode = currentNode.getLeftChildNode();
@@ -107,58 +136,37 @@ public class AvlTree implements Iterable<Integer> {
         return ERROR_VAL;
     }
 
+    /**
+     * Removes the node with the given value from the tree, if it exists.
+     * @param toDelete the value to remove from the tree.
+     * @return true if the given value was found and deleted, false otherwise.
+     */
     public boolean delete(int toDelete){
-        AvlTreeNode currentNode;
-        currentNode = findNode(toDelete);
+        // get the node in question
+        AvlTreeNode currentNode = findNode(toDelete);
 
+        // if it's null (doesn't exist), return false.
         if(currentNode == null)
             return false;
 
+        // get the parent and the left and right children of the node.
         AvlTreeNode treeToBalance = currentNode.getParentNode();
         AvlTreeNode left = currentNode.getLeftChildNode();
         AvlTreeNode right = currentNode.getRightChildNode();
         int value = currentNode.getValue();
+
+        // decrement the size.
         size--;
 
         // Case 1: If currentNode has no right child, then current's left replaces current.
         if (right == null){
-            if (treeToBalance == null){
-                rootNode = left;
-
-                if (rootNode != null)
-                    rootNode.setParentNode(null);
-            } else {
-                // if parent value is greater than current value
-                // make the current left child a left child of parent
-                if (value < treeToBalance.getValue())
-                    treeToBalance.setLeftChildNode(left);
-
-                // if parent value is less than current value
-                // make the current left child a right child of parent
-                else if (value > treeToBalance.getValue())
-                    treeToBalance.setRightChildNode(left);
-            }
+            updateTree(treeToBalance, left, value);
         // Case 2: If current's right child has no left child, then current's right child
         //         replaces current
         } else if (right.getLeftChildNode() == null) {
             right.setLeftChildNode(left);
 
-            if (treeToBalance == null){
-                rootNode = right;
-
-                if (rootNode != null)
-                    rootNode.setParentNode(null);
-            } else {
-                // if parent value is greater than current value
-                // make the current right child a left child of parent
-                if (value < treeToBalance.getValue())
-                    treeToBalance.setLeftChildNode(right);
-
-                // if parent value is less than current value
-                // make the current right child a right child of parent
-                else if (value > treeToBalance.getValue())
-                    treeToBalance.setRightChildNode(right);
-            }
+            updateTree(treeToBalance, right, value);
         }
         // Case 3: If current's right child has a left child, replace current with current's
         //         right child's left-most child
@@ -174,25 +182,15 @@ public class AvlTree implements Iterable<Integer> {
 
             // assign leftmost's left and right to current's left and right children
             leftMost.setLeftChildNode(left);
-
             leftMost.setRightChildNode(right);
 
-            if(treeToBalance == null){
-                rootNode = leftMost;
-
-                if(rootNode != null)
-                    rootNode.setParentNode(null);
-            } else {
-                if (value < treeToBalance.getValue())
-                    treeToBalance.setLeftChildNode(leftMost);
-
-                else if(value > treeToBalance.getValue())
-                    treeToBalance.setRightChildNode(leftMost);
-            }
+            updateTree(treeToBalance, leftMost, value);
         }
 
+        // if the parent node isn't null we balance the tree.
         if (treeToBalance != null)
             treeToBalance.balanceNode();
+        // else we balance the tree from the root.
         else {
             if (rootNode != null)
                 rootNode.balanceNode();
@@ -201,66 +199,117 @@ public class AvlTree implements Iterable<Integer> {
         return true;
     }
 
+    /**
+     * Updates the parent node after the removal of the unwanted node.
+     * @param parentNode the parent of our node.
+     * @param childNode on of the children of our node
+     * @param value the value of our node
+     */
+    private void updateTree(AvlTreeNode parentNode, AvlTreeNode childNode, int value) {
+        // if our node was the root, we replace the root with the given childNode.
+        if (parentNode == null){
+            rootNode = childNode;
+            // if the tree isn't empty after the assignment we update the parent property of the new root.
+            if (rootNode != null)
+                rootNode.setParentNode(null);
+
+        } else {
+            // if parent value is greater than current value
+            // make the current node child a left child of parent
+            if (value < parentNode.getValue())
+                parentNode.setLeftChildNode(childNode);
+
+            // if parent value is less than current value
+            // make the current node child a right child of parent
+            else if (value > parentNode.getValue())
+                parentNode.setRightChildNode(childNode);
+        }
+    }
+
+    /**
+     * Gets the node with the given value.
+     * @param value value to search for in the tree.
+     * @return returns the node with the given value.
+     */
     private AvlTreeNode findNode(int value){
         AvlTreeNode currentNode = rootNode;
-        int height = 0;
 
         while(currentNode != null){
+            // if currentNode value is greater than the value we are looking for, we go left in the tree.
             if(value < currentNode.getValue()){
                 currentNode = currentNode.getLeftChildNode();
-                height++;
+            // if it is greater then we go right.
             } else if (value > currentNode.getValue()){
                 currentNode = currentNode.getRightChildNode();
-                height++;
+            // if equal we exit the loop and return currentNode.
             } else break;
         }
 
         return currentNode;
     }
 
-    private ArrayList<Integer> inOrderTraversal(AvlTreeNode node, ArrayList<Integer> currentList){
+    /**
+     * This method converts the tree structure into a sorted int array.
+     * @param node node in the tree to travers from.
+     * @param currentList current int[] array that we are going to add items to
+     * @param index current index in the array.
+     * @return int[] array with all the children of a given node in ascending order.
+     */
+    protected int[] inOrderTraversal(AvlTreeNode node, int[] currentList, int index){
         if (node != null){
-            inOrderTraversal(node.getLeftChildNode(), currentList);
-            currentList.add(node.getValue());
-            inOrderTraversal(node.getRightChildNode(), currentList);
+            // first go over the left nodes
+            inOrderTraversal(node.getLeftChildNode(), currentList, index);
+            // add the current value to the array
+            currentList[++index] = node.getValue();
+            // go to the right
+            inOrderTraversal(node.getRightChildNode(), currentList, index);
         }
 
         return currentList;
     }
 
+    /**
+     * Returns the number of nodes in the tree (its size).
+     * @return the number of nodes in the tree.
+     */
     public int size(){
         return size;
     }
 
+    /**
+     * Calculates the minimum number of nodes in an AVL tree of height h.
+     * @param h the height of the tree (a non-negative number) in question.
+     * @return the minimum number of nodes in an AVL tree of the given height.
+     */
     public static int findMinNodes(int h){
-        if(h == 0)
-            return 1;
-        if(h == 1)
-            return 2;
-
-        return findMinNodes(h-1) + findMinNodes(h-2) + 1;
+        double sqrt5 = Math.sqrt(5);
+        return (int) (((2 + sqrt5) * Math.pow(1 + sqrt5, h) + (sqrt5 - 2) * Math.pow(1 - sqrt5, h))
+                / (sqrt5 * Math.pow(2, h)) - 1);
     }
 
     /**
-     * Returns an iterator over elements of type {@code T}.
+     * Returns an iterator over elements of type int.
      *
      * @return an Iterator.
      */
     @Override
     public Iterator<Integer> iterator() {
-        ArrayList<Integer> valueList = inOrderTraversal(rootNode, new ArrayList<Integer>());
+        // create a list of all elements in the tree
+        int[] valueList = inOrderTraversal(rootNode, new int[size], 0);
 
         Iterator<Integer> it = new Iterator<Integer>() {
             int currentIndex = 0;
 
             @Override
             public boolean hasNext() {
-                return currentIndex < valueList.size();
+                return currentIndex < size();
             }
 
             @Override
-            public Integer next() {
-                return valueList.get(currentIndex++);
+            public Integer next() throws NoSuchElementException {
+                if(!hasNext()) throw new NoSuchElementException();
+
+                return valueList[currentIndex++];
             }
         };
 
--- ex4/src/data_structures/oop/ex4/data_structures/AvlTreeNode.java
+++ ex4/src/data_structures/oop/ex4/data_structures/AvlTreeNode.java
@@ -1,74 +1,125 @@
 package oop.ex4.data_structures;
 /**
- * Created by jenia on 14/04/2016.
+ * This class represents a single node in the AvlTree data structure.
  */
 public class AvlTreeNode {
-
+    // an enum of all different tree states
     private enum TreeState{
         Balanced,
         LeftHeavy,
         RightHeavy
     }
 
+    /**
+     * Constants
+     */
+    public static final int ERROR_VAL = -1;
+    public static final int MAX_LEFT_BALANCE_FACTOR = -1;
+    public static final int MIN_RIGHT_BALANCE_FACTOR = 1;
+
+    /**
+     * Member variable of the node class
+     */
     private AvlTree tree;
     private AvlTreeNode parentNode;
     private AvlTreeNode rightChildNode;
     private AvlTreeNode leftChildNode;
     private int value;
 
+    /**
+     * AvlTreeNode constructor
+     * @param value the value of this node.
+     * @param tree the tree this node belongs to.
+     * @param parentNode the parent node of this node.
+     */
     public AvlTreeNode(int value, AvlTree tree, AvlTreeNode parentNode){
         this.value = value;
         this.tree = tree;
         this.parentNode = parentNode;
     }
 
+    /**
+     * @return the parent node
+     */
     public AvlTreeNode getParentNode(){ return parentNode;}
 
+    /**
+     * @return the right child node.
+     */
     public AvlTreeNode getRightChildNode(){
         return rightChildNode;
     }
 
+    /**
+     * @return the left child node.
+     */
     public AvlTreeNode getLeftChildNode(){
         return leftChildNode;
     }
 
+    /**
+     * @return the value of the node.
+     */
     public int getValue(){
         return value;
     }
 
+    /**
+     * Sets the parent node of this node.
+     * @param parentNode the parent node to assign.
+     */
     public void setParentNode(AvlTreeNode parentNode){
         this.parentNode = parentNode;
     }
 
+    /**
+     * Sets the right child node of this node.
+     * @param node the node to be assigned on the right.
+     */
     public void setRightChildNode(AvlTreeNode node){
         rightChildNode = node;
 
+        // if the child node is not null we update it's parent to be this node.
         if(node != null){
             rightChildNode.setParentNode(this);
         }
     }
 
+    /**
+     * Sets the right child node of this node.
+     * @param node the node to be assigned on the right.
+     */
     public void setLeftChildNode(AvlTreeNode node){
         leftChildNode = node;
 
+        // if the child node is not null we update it's parent to be this node.
         if(node != null){
             leftChildNode.setParentNode(this);
         }
     }
 
+    /**
+     * Sets the value of this node
+     * @param value the new value to assign.
+     */
     public void setValue(int value){
         this.value = value;
     }
 
+    /**
+     * Checks the state of this node's subtree and balances it accordingly
+     */
     public void balanceNode(){
         if(getState() == TreeState.RightHeavy) {
             if (rightChildNode != null && rightChildNode.balanceFactor() < 0) {
+                // if right child is not null and it's left heavy we rotate right and then left.
                 rotateLeftRight();
             } else {
                 rotateLeft();
             }
         } else if (getState() == TreeState.LeftHeavy){
             if(leftChildNode != null && leftChildNode.balanceFactor() > 0){
+                // if the left child is not null and it's right heavy we rotate left and the right.
                 rotateRightLeft();
             } else {
                 rotateRight();
@@ -76,75 +127,148 @@ public class AvlTreeNode {
         }
     }
 
+    /**
+     * Rotate the node left.
+     */
     private void rotateLeft(){
+        //     a
+        //      \
+        //       b
+        //        \
+        //         c
+        //
+        // becomes
+        //       b
+        //      / \
+        //     a   c
+
         AvlTreeNode newRoot = getRightChildNode();
+        // replace the current root with the new root
         replaceRoot(newRoot);
+        // take ownership of right's left child as right (now parent)
         setRightChildNode(newRoot.getLeftChildNode());
+        // the new root takes this as it's left
         newRoot.setLeftChildNode(this);
     }
 
+    /**
+     * Rotate the node right.
+     */
     private void rotateRight(){
+        //     c (this)
+        //    /
+        //   b
+        //  /
+        // a
+        //
+        // becomes
+        //       b
+        //      / \
+        //     a   c
+
         AvlTreeNode newRoot = getLeftChildNode();
+        // replace the current root with the new root
         replaceRoot(newRoot);
+        // take ownership of left's right child as left (now parent)
         setLeftChildNode(newRoot.getRightChildNode());
+        // the new root takes this as it's right
         newRoot.setRightChildNode(this);
     }
 
+    /**
+     * Rotates the right subtree right and then rotates the node left.
+     */
     private void rotateLeftRight(){
         rightChildNode.rotateRight();
         rotateLeft();
     }
 
+    /**
+     * Rotates the left subtree left and then rotates the node right.
+     */
     private void rotateRightLeft(){
         leftChildNode.rotateLeft();
         rotateRight();
     }
 
+    /**
+     * Replaces the root of the tree/subtree.
+     * @param newRoot
+     */
     private void replaceRoot(AvlTreeNode newRoot){
         if (parentNode != null){
-            //System.out.println("replacing root, parent: " + parentNode.getValue());
+            // if this node has a parent then we check in which subtree this node belongs to and replace the
+            // child of the parent with the new node.
             if (parentNode.getLeftChildNode() == this)
                 parentNode.setLeftChildNode(newRoot);
             else if (parentNode.getRightChildNode() == this)
                 parentNode.setRightChildNode(newRoot);
         } else {
+            // if this node is the root of the tree, we replace the root of the tree
             tree.setRoot(newRoot);
         }
 
+        // update the parents of this node and the new root's
         newRoot.setParentNode(parentNode);
         this.setParentNode(newRoot);
     }
 
+    /**
+     * Returns the maximum height of this node.
+     * @return the height of this node from the farthest leaf.
+     */
     public int height(){
-        return maxChildHeight(this) - 1;
+        return maxChildHeight(this);
     }
 
+    /**
+     * Recursive method to calculate the height of the given node and choose the maximum out of its left and right
+     * subtrees.
+     * @param node the node to calculate the height for.
+     * @return height of the node if it exists; -1 otherwise.
+     */
     public int maxChildHeight(AvlTreeNode node){
         if(node != null){
             return 1 + Math.max(maxChildHeight(node.getLeftChildNode()), maxChildHeight(node.getRightChildNode()));
         }
 
-        return 0;
+        return ERROR_VAL;
     }
 
+    /**
+     * Calculates the height of the left subtree
+     * @return the height of the left subtree.
+     */
     private int leftHeight(){
         return maxChildHeight(leftChildNode);
     }
 
+    /**
+     * Calculates the height of the right subtree.
+     * @return the height of the right subtree.
+     */
     private int rightHeight(){
         return maxChildHeight(rightChildNode);
     }
 
+    /**
+     * Checks the balancing state of the tree
+     * @return
+     */
     private TreeState getState(){
-        if(balanceFactor() > 1)
+        if(balanceFactor() > MIN_RIGHT_BALANCE_FACTOR)
             return TreeState.RightHeavy;
 
-        else if(balanceFactor() < -1)
+        else if(balanceFactor() < MAX_LEFT_BALANCE_FACTOR)
             return TreeState.LeftHeavy;
 
         return TreeState.Balanced;
     }
 
+    /**
+     * Calculates the balance factor of this node.
+     * @return
+     */
     private int balanceFactor(){
         return rightHeight() - leftHeight();
     }
