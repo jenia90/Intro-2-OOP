824a650c752dc41036dc8275fa62bd19e3ba5a7b
Jenia <genia90@gmail.com>
2016-05-18 00:26:28 +0300

    FUCKING LAMBDAS ALL THE WAAAAAAAAAAAY!!!!!!

--- ex5/src/fileprocessing/DirectoryProcessor.java
+++ ex5/src/fileprocessing/DirectoryProcessor.java
@@ -1,12 +1,9 @@
 package fileprocessing;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
-import java.util.stream.Stream;
-
-import fileprocessing.Components.*;
+import java.util.*;
+import java.util.function.Predicate;
+import java.util.stream.*;
 
 /**
  * Created by jenia on 10/05/2016.
@@ -14,9 +11,11 @@ import fileprocessing.Components.*;
 public class DirectoryProcessor {
     private static final int SOURSCE_DIR_IDX = 0;
     private static final int COMMAND_FILE_IDX = 1;
+    public static final int FILTER_TYPE_IDX = 0;
+    public static final int KB_CONVERSION = 1024;
 
 
-    private ArrayList<Section> sections;
+    private ArrayList<String[]> sections;
 
     public DirectoryProcessor(File commandFile) throws NullPointerException {
         if (commandFile == null)
@@ -29,14 +28,14 @@ public class DirectoryProcessor {
         }
     }
 
-    private ArrayList<Section> getSections(File commandFile) throws IOException{
+    private ArrayList<String[]> getSections(File commandFile) throws IOException{
         if (commandFile.isDirectory())
             throw new IOException("Invalid command file exception.");
 
-        ArrayList<Section> sections = new ArrayList<>();
+        ArrayList<String[]> sections = new ArrayList<>();
+        Reader reader = new FileReader(commandFile);
+        BufferedReader bufferedReader = new BufferedReader(reader);
         try {
-            Reader reader = new FileReader(commandFile);
-            BufferedReader bufferedReader = new BufferedReader(reader);
 
             Scanner s = new Scanner(commandFile);
 
@@ -45,19 +44,58 @@ public class DirectoryProcessor {
                 String[] filterRules = s.next().split("#");
                 s.next();
                 String[] orderBy = s.next().split("#");
-                sections.add(new Section(filterRules, orderBy));
             }
 
-            reader.close();
-            bufferedReader.close();
         } catch (IOException ex){
             System.err.println("Unable to read command file.\n" + ex);
+        } finally {
+            reader.close();
+            bufferedReader.close();
         }
 
         return sections;
     }
 
+    private Predicate<File> getFileFilter(String[] filterBy){
+        switch (filterBy[FILTER_TYPE_IDX]){
+            case "greater_than":
+                Predicate<File> greaterThan =
+                        file -> file.isFile() && file.length() / KB_CONVERSION >= Integer.parseInt(filterBy[1]);
+
+                return filterBy[2].equals("NOT") ? greaterThan.negate() : greaterThan;
+            case "between":
+                Predicate<File> betweenFilter =
+                        file -> file.isFile() && (file.length() / KB_CONVERSION >= Integer.parseInt(filterBy[1]) &&
+                        file.length() / KB_CONVERSION <= Integer.parseInt(filterBy[2]));
+
+                return filterBy[3].equals("NOT") ? betweenFilter.negate() : betweenFilter;
+            case "smaller_than":
+                Predicate<File> smallerThan =
+                        file -> file.isFile() && file.length() / KB_CONVERSION <= Integer.parseInt(filterBy[1]);
+
+                return filterBy[2].equals("NOT") ? smallerThan.negate() : smallerThan;
+            case "file":
+                break;
+            case "contains":
+                break;
+            case "prefix":
+                break;
+            case "suffix":
+                break;
+            case "writeable":
+                break;
+            case "executable":
+                break;
+            case "hidden":
+                break;
+        }
+
+        return null;
+    }
+
     private void processDirectory(String path){
+        File file = new File(path);
+        File[] filteredFiles = file.listFiles();
 
     }
 
